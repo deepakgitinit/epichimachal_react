b273e971737726c25cfd3f7171387a75ad2426d1
Deepak <deepakhomemail@gmail.com>
2024-03-24 23:24:30 +0530

    Version 0.0.3 - Contact Updated

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -131,15 +131,15 @@ const [contactForm, setContactForm] = useState({
                     <div className="grid grid-cols-1 gap-2 lg:grid-cols-2 place-items-center text-sm *:m-2 justify-center items-center mt-4">
                         <div className="flex items-center *:mx-1">
                             <img className="size-4 object-cover rounded-full" src="https://images.rawpixel.com/image_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjkzMi1uaW5nLTEwNS1rbGMzZzdoYS5qcGc.jpg" />
-                            <a href="tel:9051290512"> 9051290512</a>
+                            <a href="tel:7580088818"> 7580088818</a>
                         </div>
                         <div className="flex items-center *:mx-1">
                             <img className="size-4 object-cover rounded-full" src="https://images.rawpixel.com/image_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjkzMi1uaW5nLTEwNS1rbGMzZzdoYS5qcGc.jpg" />
-                            <a href="tel:9051290512"> 9051290512</a>
+                            <a href="tel:9317632618"> 9317632618</a>
                         </div>
                         <div className="flex items-center *:mx-1">
                             <img className="size-4 object-cover rounded-full" src="https://static-00.iconduck.com/assets.00/whatsapp-icon-1024x1024-cilsjgvb.png" />
-                            <a href="https://wa.me/9051290512"> 9051290512</a>
+                            <a href="https://wa.me/9888488818"> 9888488818</a>
                         </div>
                         <div className="flex items-center *:mx-1">
                             <img className="size-4 object-cover rounded-full" src="https://static.vecteezy.com/system/resources/thumbnails/005/269/576/small/mail-icon-free-vector.jpg" />
